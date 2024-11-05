375d1a76bc3e3529bd234f283c5e88306d7210c2
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-11-05 10:43:02 -0500

    trabajando con lista en java

---
+++ src/com/aluracursos/listas/modelo/Producto.java
@@ -0,0 +1,4 @@
+package com.aluracursos.listas.modelo;
+
+public class Producto {
+}
---
+++ src/com/aluracursos/listas/principal/Principal.java
@@ -0,0 +1,7 @@
+package com.aluracursos.listas.principal;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
